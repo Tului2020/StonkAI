397ea4ba03f039d0ed6dd63995b2b23b49bf6c59
Tului Gantulga <tgantulga@ucdavis.edu>
2021-07-03 19:47:47 -0700

    initial files

---
+++ Javascript/entry.js
@@ -0,0 +1,28 @@
+
+
+// grabbing elemets
+let inputClick = document.getElementById("input-click");
+
+
+
+// FUNCTIONS
+const inputClickHandle = () => {
+  console.log("clicked")
+}
+
+
+
+
+
+
+
+
+
+// Connections
+inputClick.onclick = inputClickHandle;
+
+
+
+
+
+
